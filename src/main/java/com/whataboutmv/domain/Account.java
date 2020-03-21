9e7e54a1f019174d14f06b249a82adcd5ed06b37
BaekSeungYeol <beck33333@naver.com>
2020-03-22 00:07:09 +0900

    init commit

---
+++ src/main/java/com/whataboutmv/domain/Account.java
@@ -0,0 +1,59 @@
+package com.whataboutmv.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    // 이메일 인증절차 판단
+    private boolean emailVerified;
+
+    // 이메일을 검증하기 위한 토큰 값
+    private String emailCheckToken;
+
+    //인증된 사용자는 그때 가입이 된 것으로 확인
+    private LocalDateTime joinedAt;
+    //자기 소개
+    private String bio;
+    // 웹 사이트 URL
+    private String url;
+    // 선호하는 영화
+    private String preferenceMovie;
+    // 좋아하는 영화배우
+    private String preferenceActor;
+    // 살고있는 지역
+    private String location;
+
+    // 프로필 이미지는 페치 모드로
+    @Lob @Basic(fetch= FetchType.EAGER)
+    private String profileImage;
+
+    private boolean comuCreatedByEmail;
+
+    private boolean comuCreatedByWeb;
+
+    private boolean comuEnrollmentResultByEmail;
+
+    private boolean comuEnrollmentResultByWeb;
+    // 바뀐 정보를 받을 것인가
+    private boolean comuUpdatedByEmail;
+
+    private boolean studyUpdateByWeb;
+}
+
