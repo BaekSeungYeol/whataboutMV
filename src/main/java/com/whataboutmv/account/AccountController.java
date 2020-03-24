7054b0af9e0bd48b34174285737cb1d978448ed2
BaekSeungYeol <beck33333@naver.com>
2020-03-25 00:52:11 +0900

    create sign-up validation

--- src/main/java/com/whataboutmv/account/AccountController.java
+++ src/main/java/com/whataboutmv/account/AccountController.java
@@ -1,17 +1,41 @@
 package com.whataboutmv.account;
 
 import com.whataboutmv.domain.Account;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
 
-        @GetMapping("/sign-up")
-        public String signUpForm(Model model) {
+    private final SignUpFormValidator signUpFormValidator;
 
-            model.addAttribute(new SignUpForm());
-            return "account/sign-up";
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
+
+    @GetMapping("/sign-up")
+    public String signUpForm(Model model) {
+
+        model.addAttribute(new SignUpForm());
+        return "account/sign-up";
+    }
+
+        @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+            if (errors.hasErrors()) {
+                return "account/sign-up";
+            }
+            // TODO 회원 가입 처리
+            return "redirect:/";
         }
 }
