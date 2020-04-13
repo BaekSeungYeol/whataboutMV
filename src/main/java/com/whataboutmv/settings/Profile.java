36e48ac38a7acb4f74a80ce7f9727df97513f7ba
BaekSeungYeol <beck33333@naver.com>
2020-04-13 16:22:49 +0900

    Profile view

---
+++ src/main/java/com/whataboutmv/settings/Profile.java
@@ -0,0 +1,27 @@
+package com.whataboutmv.settings;
+
+
+import com.whataboutmv.domain.Account;
+import lombok.Data;
+
+@Data
+public class Profile {
+    //자기 소개
+    private String bio;
+    // 웹 사이트 URL
+    private String url;
+    // 선호하는 영화
+    private String preferenceKind;
+    // 좋아하는 영화배우
+    private String preferenceActor;
+    // 살고있는 지역
+    private String location;
+
+    public Profile(Account account) {
+        this.bio = account.getBio();
+        this.url = account.getUrl();
+        this.preferenceKind = account.getPreferenceKind();
+        this.preferenceActor = account.getPreferenceActor();
+        this.location = account.getLocation();
+    }
+}
