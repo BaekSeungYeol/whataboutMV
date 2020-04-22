aeea6a5e1d550ad261407c53461cb1322de57a80
BaekSeungYeol <beck33333@naver.com>
2020-04-22 15:48:36 +0900

    Zone update

---
+++ src/main/java/com/whataboutmv/zone/ZoneRepository.java
@@ -0,0 +1,8 @@
+package com.whataboutmv.zone;
+
+import com.whataboutmv.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+
+}
