9cd2f3720bbe25dd163686f8086f1682809abfbe
BaekSeungYeol <beck33333@naver.com>
2020-05-23 13:48:21 +0900

    Fetch join & solve JPA N+1

--- src/main/java/com/whataboutmv/modules/movie/MovieRepositoryExtensionImpl.java
+++ src/main/java/com/whataboutmv/modules/movie/MovieRepositoryExtensionImpl.java
@@ -1,6 +1,9 @@
 package com.whataboutmv.modules.movie;
 
 import com.querydsl.jpa.JPQLQuery;
+import com.whataboutmv.modules.account.QAccount;
+import com.whataboutmv.modules.tag.QTag;
+import com.whataboutmv.modules.zone.QZone;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import java.util.List;
@@ -18,8 +21,11 @@ public class MovieRepositoryExtensionImpl extends QuerydslRepositorySupport impl
         JPQLQuery<Movie> query = from(movie).where(movie.published.isTrue()
                 .and(movie.title.containsIgnoreCase(keyword))
                 .or(movie.tags.any().title.containsIgnoreCase(keyword))
-                .or(movie.zones.any().localNameOfCity.containsIgnoreCase(keyword)));
-
+                .or(movie.zones.any().localNameOfCity.containsIgnoreCase(keyword)))
+                .leftJoin(movie.tags, QTag.tag).fetchJoin()
+                .leftJoin(movie.zones, QZone.zone).fetchJoin()
+                .leftJoin(movie.members, QAccount.account).fetchJoin()
+                .distinct();
         return query.fetch();
     }
 }
